bf0fd6ae751e08f69b5aac0006c3d7c9c008014e
James Collins <collijk@uw.edu>
2025-02-16 13:29:22 -0800

    Updated CRS definitions

--- src/rra_building_density/constants.py
+++ src/rra_building_density/constants.py
@@ -121,6 +121,7 @@ GHSL_VERSIONS = {
 
 class CRS(BaseModel):
     name: str
+    short_name: str
     bounds: tuple[float, float, float, float]
     code: str = ""
     proj_string: str = ""
@@ -144,54 +145,70 @@ class CRS(BaseModel):
                 raise ValueError(msg)
         return self
 
+    def to_string(self) -> str:
+        if self.code:
+            return self.code
+        return self.proj_string
+
     def to_pyproj(self) -> pyproj.CRS:
         if self.code:
             return pyproj.CRS.from_user_input(self.code)
         return pyproj.CRS.from_user_input(self.proj_string)
 
+    def __hash__(self) -> int:
+        return hash(self.name)
+
 
 CRSES: dict[str, CRS] = {
     "wgs84": CRS(
         name="WGS84",
+        short_name="wgs84",
         code="EPSG:4326",
         proj_string="+proj=longlat +datum=WGS84 +no_defs +type=crs",
         bounds=(-180.0, -90.0, 180.0, 90.0),
     ),
     "wgs84_anti_meridian": CRS(
         name="WGS84 Anti-Meridian",
+        short_name="wgs84_am",
         proj_string="+proj=longlat +lon_0=180 +datum=WGS84 +no_defs +type=crs",
         bounds=(-180.0, -90.0, 180.0, 90.0),
     ),
     "itu_anti_meridian": CRS(
         name="PDC Mercator",
+        short_name="itu_am",
         code="EPSG:3832",
         proj_string="+proj=merc +lon_0=150 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +type=crs",
         bounds=(-5711803.07, -8362698.55, 15807367.69, 10023392.49),
     ),
     "mollweide": CRS(
         name="Mollweide",
+        short_name="mollweide",
         code="ESRI:54009",
         proj_string="+proj=moll +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +type=crs",
         bounds=(-18040095.7, -9020047.85, 18040095.7, 9020047.85),
     ),
     "mollweide_anti_meridian": CRS(
         name="Mollweide Anti-Meridian",
+        short_name="mollweide_am",
         proj_string="+proj=moll +lon_0=180 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +type=crs",
         bounds=(-18040095.7, -9020047.85, 18040095.7, 9020047.85),
     ),
     "world_cylindrical": CRS(
         name="World Cylindrical",
+        short_name="world_cylindrical",
         code="ESRI:54034",
         proj_string="+proj=cea +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +type=crs",
         bounds=(-20037508.34, -6363885.33, 20037508.34, 6363885.33),
     ),
     "world_cylindrical_anti_meridian": CRS(
         name="World Cylindrical Anti-Meridian",
+        short_name="world_cylindrical_am",
         proj_string="+proj=cea +lat_ts=0 +lon_0=180 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +type=crs",
         bounds=(-20037508.34, -6363885.33, 20037508.34, 6363885.33),
     ),
     "web_mercator": CRS(
         name="Web Mercator",
+        short_name="web_mercator",
         code="EPSG:3857",
         proj_string="+proj=merc +a=6378137 +b=6378137 +lat_ts=0 +lon_0=0 +x_0=0 +y_0=0 +k=1 +units=m +nadgrids=@null +wktext +no_defs +type=crs",
         bounds=(-20037508.34, -20048966.1, 20037508.34, 20048966.1),
