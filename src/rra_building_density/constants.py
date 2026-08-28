035f954e1e39c642fb602928bd73264900dca0d2
Ryan Barber <rmbarber@uw.edu>
2026-08-27 19:27:43 -0700

    increase runtime

--- src/rra_building_density/constants.py
+++ src/rra_building_density/constants.py
@@ -48,8 +48,8 @@ class MicrosoftVersion(BuiltVersion):
 
     def process_resources(self, resolution: str) -> tuple[str, str]:
         return {
-            RESOLUTIONS.r40: ("6G", "10m"),
-            RESOLUTIONS.r100: ("6G", "15m"),
+            RESOLUTIONS.r40: ("6G", "30m"),
+            RESOLUTIONS.r100: ("6G", "45m"),
         }[RESOLUTIONS(resolution)]
 
 
